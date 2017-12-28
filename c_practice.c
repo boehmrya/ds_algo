b2659fbfb37bba864b42e278ec5143f623fb34d7
Ryan Boehm <ryan.w.boehm@gmail.com>
2017-12-28 08:20:13 -0600

    unique characters function

---
+++ c_practice.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/* 
+TO COMPILE: clang c_practice.c -o c_practice
+TO RUN: ./c_practice
+*/
+
+
+void reverse(char* str) {
+	
+}
+
+int main() {
+	
+	return 0;
+}
--- ch1.java
+++ ch1.java
@@ -7,14 +7,30 @@
  */
 public class ch1 {
 
-
+	/*
+	* determines if a string has unique characters
+	*/
+	public static boolean uniqueChars( String word ) {
+		for (int i = 0; i < word.length(); i++) {
+            char c1 = word.charAt(i);
+            for (int j = (i + 1); j < word.length(); j++) {
+            	char c2 = word.charAt(j);
+               	if (c1 == c2) {
+               		return false;
+               	}
+       		}    
+       	}
+       	return true;
+	}
 
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-	    
-	    
+	    String testWord1 = "helloWorld";
+	    String testWord2 = "heloWrd";
+	    System.out.println("Test Word 1: " + uniqueChars(testWord1));
+	    System.out.println("Test Word 2: " + uniqueChars(testWord2));
 	}
 
 }
