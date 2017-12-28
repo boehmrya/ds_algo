ee7b1b35ca14841de21b11c8016a6858df14e404
Ryan Boehm <ryan.w.boehm@gmail.com>
2017-12-28 10:03:10 -0600

    reversed a string in place

--- c_practice.c
+++ c_practice.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <string.h>
 
 /* 
 TO COMPILE: clang c_practice.c -o c_practice
@@ -8,10 +9,23 @@ TO RUN: ./c_practice
 
 
 void reverse(char* str) {
-	
+	int i = 0;
+	int j = strlen(str) - 1;
+	char c;
+
+
+	while (i < j) {
+		char c = str[i];
+		str[i] = str[j];
+		str[j] = c; 
+		j--;
+		i++;
+	}
 }
 
 int main() {
-	
+	char str[] = "hello";
+	reverse(str);
+	printf("reversed string: %s\n", str);
 	return 0;
 }
